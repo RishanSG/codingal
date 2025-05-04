b56abcf7d0bcfc34cb662a26654fe58f8aaf189c
rishan <rzspro8@gmail.com>
2025-05-05 00:35:38 +0530

    birthday project

---
+++ python/class 2/Birthday saving.py
@@ -0,0 +1,7 @@
+Rohan="5/6/15"
+Ram="7/12/10"
+Dheer="6/11/10"
+Rishan="17/5/11"
+Sandeep="31/12/09"
+
+print(f"Rishan's birthday is on {Rishan}, Sandeep's birthday is on {Sandeep}, Ram's birthday is on {Ram},Dheer's birthday is on {Dheer},Rohan's birthday is on {Rohan}")
