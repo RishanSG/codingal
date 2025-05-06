1cebed9404949408a6fe4d9e3edf2b3167af79e0
rishan <rzspro8@gmail.com>
2025-05-06 19:18:18 +0530

    learning typecasting

---
+++ python/class 3/typecasting.py
@@ -0,0 +1,17 @@
+x=int(input("Enter a Number: "))
+y=int(input("Enter another Number: "))
+
+print(f"the value of x is {x}")
+print(f"type of x is {type(x)}")
+print(f"addition of x and y is {x+y}")
+
+
+# a character string cannot be converted to an integer
+
+x2=False 
+y2=int(x2)
+print(f"y2 value is {y2}")
+
+x3=-12
+y3=bool(x3)
+print(f"y3 value is {y3}")
