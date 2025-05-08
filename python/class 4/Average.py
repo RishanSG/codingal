22dfcf98de26ce92607cb6f6dd920b65e5675c10
rishan <rzspro8@gmail.com>
2025-05-08 19:05:10 +0530

    average

---
+++ python/class 4/Average.py
@@ -0,0 +1,11 @@
+print("let us calculate the average height of the trees in his backyard")
+x=int(input("first tree:"))
+y=int(input("second tree:"))
+z=int(input("third tree:"))
+a=int(input("fourth tree:"))
+b=int(input("fifth tree:"))
+
+x1=a+b+x+y+z
+x2=x1/5
+
+print(f"average of the height of trees is {x2}")
