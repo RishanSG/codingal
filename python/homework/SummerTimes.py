6335ea0ab605f7f332789f2c951fd40bf1bc6229
rishan <rzspro8@gmail.com>
2025-05-15 19:07:57 +0530

    Is and Is Not

---
+++ python/homework/SummerTimes.py
@@ -0,0 +1,7 @@
+temp=int(input("Enter Temperature"))
+if 0<=temp<=9:
+    print("Extremely Cold, wear thick dark Jackets, gloves and use room heaters")
+elif 10<=temp<=29:
+    print("Cold, wear a jacket and wear dark woollen clothes")
+elif 30<=temp<=45:
+    print("Hot temperature, wear cotton light coloured clothes")
