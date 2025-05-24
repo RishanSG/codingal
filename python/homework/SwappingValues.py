e764a02efeb1f4f8d3ebed938827b61f3dcc6944
rishan <rzspro8@gmail.com>
2025-05-24 18:52:50 +0530

    hw

---
+++ python/homework/SwappingValues.py
@@ -0,0 +1,13 @@
+x=int(input("Enter 1st number: "))
+y=int(input("Enter 2nd number: "))
+z=int(input("Enter 3rd number: "))
+
+x1=x
+y1=y
+z1=z
+
+x=y1
+y=z1
+z=x1
+
+print(f"Swapped Values are first number-{x}, second number-{y}, third number-{z}")
