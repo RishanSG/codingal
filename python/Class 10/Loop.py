e9a9f7a4c5118b5a4cfb191e0d664f7110ca9bee
rishan <rzspro8@gmail.com>
2025-05-24 17:56:17 +0530

    homework

---
+++ python/Class 10/Loop.py
@@ -0,0 +1,8 @@
+#for i in range(100,301,1):
+#    print("Rishan i:", i)
+
+#for i in range (17,171,17):
+#   print(i)
+
+for i in range(1,11):
+    print(f"17x {i}={17*i}")
---
+++ python/homework/SwappingValues.py
@@ -0,0 +1,4 @@
+number=int(input("Give number:"))
+power=int(input("give power"))
+final_answer=number**power
+print(f"Value is {final_answer}")
