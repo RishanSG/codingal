9ab92cc7f8bf67f5ed1f12aaaa5e8a5d746563a2
rishan <rzspro8@gmail.com>
2025-06-07 19:08:07 +0530

    lambda

---
+++ python/Class 16/AdvancedFucntion.py
@@ -0,0 +1,6 @@
+def add (*arg):
+    for i in arg :
+        print(i)
+add(1,23,4,555,565,789,88)
+print("--------")
+add(1,59,14,789,554)
---
+++ python/Class 16/lambda.py
@@ -0,0 +1,6 @@
+def add (*arg):
+    for i in arg :
+        print(i)
+add=lambda a,b: a+b
+
+print(add(10,20))
