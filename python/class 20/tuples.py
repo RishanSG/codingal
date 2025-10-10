8b1c867c3378608d1842333e93d1b982106d2aca
rishan <rzspro8@gmail.com>
2025-10-10 18:45:26 +0530

    Tuples

---
+++ python/class 20/tuples.py
@@ -0,0 +1,11 @@
+names=["akash", "rishan", "rohit", [1,2,3,4,'element']]
+print(names[3][4])
+
+names.append("Rohan")
+print(names)
+
+names.remove("akash")
+print(names)
+
+t=("akash", "rishan", "rohit", (1,2,3,4))
+print(t[3][0])
