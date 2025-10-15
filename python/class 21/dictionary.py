94444c77b78af3713d3e1a3db3e9f4e8432ed870
rishan <rzspro8@gmail.com>
2025-10-15 18:29:26 +0530

    dictionaries

---
+++ python/class 21/dictionary.py
@@ -0,0 +1,10 @@
+weatherinfo= {
+"temp" : 275.45 ,
+"feels like" : 271.7 ,
+"pressure" : 1014 ,
+"humidity" : 74 ,
+"temp_min" : 274.27 ,
+"temp_max" : 276.48
+
+}
+print ("current temperature is ", weatherinfo["temp"],"kelvin.")
